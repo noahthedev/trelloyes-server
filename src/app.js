ddcc3ca27cb1ce5eecebc3ec3581e8e58aa71bb0
Noah Roberts <noahmroberts@gmail.com>
2021-04-28 14:52:12 -0500

    updated gitignore

--- src/app.js
+++ src/app.js
@@ -4,14 +4,51 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const winston = require('winston')
 
 const app = express()
-
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+const cards = [{
+  id: 1,
+  title: 'Task One',
+  content: 'This is card one'
+}];
+const lists = [{
+  id: 1,
+  header: 'List One',
+  cardIds: [1]
+}];
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')

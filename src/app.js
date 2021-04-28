a4575dde2ce23a55e88e134c543c2ee6a3a56b42
Noah Roberts <noahmroberts@gmail.com>
2021-04-28 18:01:24 -0500

    end of day

--- src/app.js
+++ src/app.js
@@ -127,6 +127,102 @@ app.post('/card', (req, res) => {
     .json(card);
 })
 
+app.post('/list', (req, res) => {
+  const { header, cardIds = [] } = req.body;
+
+  if (!header) {
+    logger.error(`Header is required`);
+    return res
+      .status(400)
+      .send('Invalid data');
+  }
+  
+  // check card IDs
+  if (cardIds.length > 0) {
+    let valid = true;
+    cardIds.forEach(cid => {
+      const card = cards.find(c => c.id == cid);
+      if (!card) {
+        logger.error(`Card with id ${cid} not found in cards array.`);
+        valid = false;
+      }
+    });
+
+    if (!valid) {
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+  }
+
+  const id = uuid();
+
+  const list = {
+    id,
+    header,
+    cardIds
+  };
+
+  lists.push(list);
+
+  logger.info(`List with id ${id} created`);
+
+  res
+    .status(201)
+    .location(`http://localhost:8000/list/${id}`)
+    .json({id});
+});
+
+app.delete('/list/:id', (req, res) => {
+  const { id } = req.params;
+
+  const listIndex = lists.findIndex(li => li.id == id);
+
+  if (listIndex === -1) {
+    logger.error(`List with id ${id} not found.`);
+    return res
+      .status(404)
+      .send('Not found');
+  }
+
+  lists.splice(listIndex, 1);
+
+  logger.info(`List with id ${id} not found.`);
+  res
+    .status(204)
+    .end();
+})
+
+app.delete('/card/:id', (req, res) => {
+  const { id } = req.params;
+
+  const cardIndex = cards.findIndex(c => c.id == id);
+
+  if (cardIndex === -1) {
+    logger.error(`Card with id ${id} not found.`);
+    return res
+      .status(404)
+      .send('Not found');
+  }
+
+  //remove card from lists
+  //assume cardIds are not duplicated in the cardIds array
+  lists.forEach(list => {
+    const cardIds = list.cardIds.filter(cid => cid  !== id);
+    list.cardIds = cardIds; 
+  })
+
+  cards.splice(cardIndex, 1)
+
+  logger.info(`Card with id ${id} deleted.`);
+
+  res
+    .status(204)
+    .end();
+})
+
+
+
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
